d6ef0e2060638945fab002722389549a98dcf947
Sanjay Manoj <sanjaymanoj04@gmail.com>
2024-06-11 22:36:28 -0500

    implemeted api functions for adding new task and getting all tasks

---
+++ backend/cmd/scheduler/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/LordSunchips/intelligent-task-scheduler/backend/internal/controller"
+	"github.com/LordSunchips/intelligent-task-scheduler/backend/internal/service"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	taskService := service.NewTaskService()
+	taskController := controller.NewTaskController(taskService)
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/tasks", taskController.GetTasksHandler).Methods("GET")
+	router.HandleFunc("api/tasks", taskController.AddTaskHandler).Methods("POST")
+
+	http.ListenAndServe(":8080", router)
+}
---
+++ backend/internal/controller/taskController.go
@@ -0,0 +1,45 @@
+package controller
+
+import (
+	"encoding/json"
+	"net/http"
+	"github.com/LordSunchips/intelligent-task-scheduler/backend/internal/service"
+)
+
+// TaskController handles HTTP requests related to tasks
+type TaskController struct {
+	TaskService *service.TaskService
+}
+
+// NewTaskController creates a new instance of TaskController
+func NewTaskController(ts *service.TaskService) *TaskController {
+	return &TaskController{
+		TaskService: ts,
+	}
+}
+
+// AddTaskHandler handles HTTP requests to add a new task
+func (tc *TaskController) AddTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var requestBody struct {
+		Name string `json:"name"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&requestBody)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	taks := tc.TaskService.AddTask(requestBody.Name)
+	
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(task)
+}
+
+// GetTasksHandler handles HTTP requests to get all tasks
+func (tc *TaskController) GetTasksHandler(w http.ResponseWriter, r *http.Request) {
+	tasks := tc.TaskService.GetTasks()
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
+}
