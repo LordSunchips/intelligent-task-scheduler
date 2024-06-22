c77cebeca1d5b660e9f6c7bff34eae28fef9c08a
Sanjay Manoj <sanjaymanoj04@gmail.com>
2024-06-22 16:52:27 -0500

    added fields to task object

--- backend/internal/model/task.go
+++ backend/internal/model/task.go
@@ -4,4 +4,7 @@ type Task struct {
 	ID string `json:"id"`
 	Name string `json:"name"`
 	Status string `json:"status"`
+	Priority int `json:"priority"`
+	Deadline string `json:"deadline"`
+	ResourceNeeded int `json:"resource_needed"`
 }
